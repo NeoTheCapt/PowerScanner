931658500f123b0b5d39c704496db19650e3b769
usualwyy <usualwyy@163.com>
2021-01-25 03:12:42 +0800

    修正bug

--- src/main/java/BrianW/AKA/BigChan/Handlers/SessionHandler.java
+++ src/main/java/BrianW/AKA/BigChan/Handlers/SessionHandler.java
@@ -1,5 +1,6 @@
 package BrianW.AKA.BigChan.Handlers;
 
+import BrianW.AKA.BigChan.Tools.Global;
 import BrianW.AKA.BigChan.Tools.utils;
 import burp.*;
 
@@ -35,42 +36,46 @@ public class SessionHandler implements ISessionHandlingAction {
 //		callbacks.printOutput("currentRequest.getRequest().length=" + currentRequest.getRequest().length);
 //		callbacks.printOutput("requestInfo.getBodyOffset()" + requestInfo.getBodyOffset());
 		System.arraycopy(currentRequest.getRequest(), requestInfo.getBodyOffset(), reqBody, 0, bodySize);
-		headers.removeIf(n -> (n.startsWith("x-originating-IP: ")));
-		headers.removeIf(n -> (n.startsWith("x-forwarded-for: ")));
-		headers.removeIf(n -> (n.startsWith("x-remote-IP: ")));
-		headers.removeIf(n -> (n.startsWith("x-remote-addr: ")));
-		headers.removeIf(n -> (n.startsWith("X-Client-IP: ")));
-		headers.add("x-originating-IP: " + randomIP);
-		headers.add("x-forwarded-for: " + randomIP);
-		headers.add("x-remote-IP: " + randomIP);
-		headers.add("x-remote-addr: " + randomIP);
-		headers.add("X-Client-IP: " + randomIP);
-		
-		headers.removeIf(n -> (n.startsWith("Cookie: ")));
-		headers.add("Cookie: ");
-		
-		headers.removeIf(n -> (n.startsWith("User-Agent: ")));
-		headers.add(String.format("User-Agent: %s/%d.0 (Windows NT %d.0; Win%d; x64) AppleWebKit/%d (%s, like %s) %s/%d.0.%d.%d %s/%d.%d",
-				utils.getRandomString(7),
-				utils.getRandomInt(20, 1),
-				utils.getRandomInt(20, 1),
-				utils.getRandomInt(100, 1),
-				utils.getRandomInt(1000, 1),
-				utils.getRandomString(5),
-				utils.getRandomString(5),
-				utils.getRandomString(6),
-				utils.getRandomInt(100, 1),
-				utils.getRandomInt(100, 1),
-				utils.getRandomInt(100, 1),
-				utils.getRandomString(6),
-				utils.getRandomInt(1000, 1),
-				utils.getRandomInt(1000, 1)
-				));
-		
-		headers.removeIf(n -> (n.startsWith("Host: ")));
-		headers.add("Host: " + currentRequest.getHttpService().getHost() + ".:" + utils.getRandomInt(65535, 1));
+		if (Global.config.getConfigRandomIPEnable_value()) {
+			headers.removeIf(n -> (n.startsWith("x-originating-IP: ")));
+			headers.removeIf(n -> (n.startsWith("x-forwarded-for: ")));
+			headers.removeIf(n -> (n.startsWith("x-remote-IP: ")));
+			headers.removeIf(n -> (n.startsWith("x-remote-addr: ")));
+			headers.removeIf(n -> (n.startsWith("X-Client-IP: ")));
+			headers.add("x-originating-IP: " + randomIP);
+			headers.add("x-forwarded-for: " + randomIP);
+			headers.add("x-remote-IP: " + randomIP);
+			headers.add("x-remote-addr: " + randomIP);
+			headers.add("X-Client-IP: " + randomIP);
+		}
+		if (Global.config.getConfigClearCookieEnable_value()) {
+			headers.removeIf(n -> (n.startsWith("Cookie: ")));
+			headers.add("Cookie: ");
+		}
+		if (Global.config.getConfigRandomUAEnable_value()) {
+			headers.removeIf(n -> (n.startsWith("User-Agent: ")));
+			headers.add(String.format("User-Agent: %s/%d.0 (Windows NT %d.0; Win%d; x64) AppleWebKit/%d (%s, like %s) %s/%d.0.%d.%d %s/%d.%d",
+					utils.getRandomString(7),
+					utils.getRandomInt(20, 1),
+					utils.getRandomInt(20, 1),
+					utils.getRandomInt(100, 1),
+					utils.getRandomInt(1000, 1),
+					utils.getRandomString(5),
+					utils.getRandomString(5),
+					utils.getRandomString(6),
+					utils.getRandomInt(100, 1),
+					utils.getRandomInt(100, 1),
+					utils.getRandomInt(100, 1),
+					utils.getRandomString(6),
+					utils.getRandomInt(1000, 1),
+					utils.getRandomInt(1000, 1)
+			));
+		}
+		if (Global.config.getConfigRandomHostEnable_value()) {
+			headers.removeIf(n -> (n.startsWith("Host: ")));
+			headers.add("Host: " + currentRequest.getHttpService().getHost() + ".:" + utils.getRandomInt(65535, 1));
+		}
 		newRequest = helpers.buildHttpMessage(headers, reqBody);
-		
 		currentRequest.setRequest(newRequest);
 	}
 }
